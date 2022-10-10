28238db53e0d85c44dca86d7ac759b89b4c62365
SamBlakely47 <blakelysam379@gmail.com>
2022-10-11 08:27:54 +1300

    Revert "Fetching Character Changes"
    This reverts commit 125b89f0b1c1eb5e981cfc8dc6591eaca87ba4f2.

--- Assets/Scripts/SanityMeter.cs
+++ Assets/Scripts/SanityMeter.cs
@@ -5,11 +5,11 @@ public class SanityMeter : MonoBehaviour
 {
     [SerializeField] private Slider m_SanityMeter;
     private bool m_DecreaseSlider = false;
-    private CharacterController m_Player;
+    private Player m_Player;
 
     private void Awake()
     {
-        m_Player = FindObjectOfType<CharacterController>();
+        m_Player = FindObjectOfType<Player>();
     }
 
     private void Start()
