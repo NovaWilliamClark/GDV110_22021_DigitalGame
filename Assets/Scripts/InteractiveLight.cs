125b89f0b1c1eb5e981cfc8dc6591eaca87ba4f2
SamBlakely47 <blakelysam379@gmail.com>
2022-10-11 08:27:04 +1300

    Fetching Character Changes

---
+++ Assets/Scripts/InteractiveLight.cs
@@ -0,0 +1,44 @@
+﻿/*******************************************************************************************
+*
+*    File: InteractiveLight.cs
+*    Purpose: Provides a light that the player can turn on
+*    Author: Sam Blakely
+*    Date: 10/10/2022
+*
+**********************************************************************************************/
+
+
+
+using TMPro;
+using UnityEngine;
+
+public class InteractiveLight : Light
+{
+    [SerializeField] private GameObject lights;
+    [SerializeField] private Canvas prompt;
+    private bool isLit = false;
+
+
+    protected override void OnTriggerEnter2D(Collider2D other)
+    {
+        if (isLit)
+        {
+            base.OnTriggerEnter2D(other);
+        }
+
+        prompt.GetComponent<TMP_Text>().text = "Interact - " + Input.GetButton("Submit");
+        prompt.gameObject.SetActive(true);
+    }
+
+    protected override void OnTriggerExit2D(Collider2D other)
+    {
+        if (isLit)
+        {
+            base.OnTriggerExit2D(other);
+        }
+        else
+        {
+            prompt.gameObject.SetActive(false);
+        }
+    }
+}
