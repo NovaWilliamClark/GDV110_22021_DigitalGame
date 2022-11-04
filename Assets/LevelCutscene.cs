33ee4e55c9c8680c0e90398917396e8b7964002d
Josh Taylor <josh@kyuu.nz>
2022-11-04 14:56:49 +1300

    Opening Cutscene

---
+++ Assets/LevelCutscene.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelCutscene : MonoBehaviour
+{
+    // manages hooks for PlayableDirector and cutscene
+
+    public void WaitForInput()
+    {
+        
+    }
+
+    public void ShowCutsceneDialogue()
+    {
+        //var dialogue = GetComponent<>()
+    }
+}
--- Assets/Scripts/UI/UIMainMenuController.cs
+++ Assets/Scripts/UI/UIMainMenuController.cs
@@ -2,12 +2,15 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.Playables;
 
 public class UIMainMenuController : MonoBehaviour
 {
     private PlayableDirector timeline;
 
+    private UnityAction onCompleteCallback;
+    
     private void Awake()
     {
         timeline = GetComponent<PlayableDirector>();
