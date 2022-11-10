b818b03aa1f214dddd7c201676d94de4f0a70042
William Clark <williamclark10090@gmail.com>
2022-11-11 11:29:35 +1300

    SM01 Chase Fixed (No Attack)

--- Assets/Scripts/AI/SM01.cs
+++ Assets/Scripts/AI/SM01.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections;
 using Character;
 using Core;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.Rendering.Universal;
 using Random = UnityEngine.Random;
 
@@ -20,12 +22,19 @@ namespace AI
         [SerializeField] private LayerMask lightMask;
         private Vector3 startPosition;
 
-        private Collider2D bodyCollider;
-        
-        [Header("Movement")]
-        private Vector2 direction;
-        private bool shouldMove;
+        [SerializeField] private Vector2 boxCastSize;
 
+        private Collider2D bodyCollider;
+
+        [Header("Movement")]
+        private Vector2 direction = new (-1, 1);
+        private bool shouldMove = true;
+
+        private RaycastHit2D boxCastHit;
+
+        private GameObject targetPlayer;
+
+        private bool isPlayerInRange = false;
         private bool isAttacking;
         private bool isAtWaypointStop;
         private int currentPoint;
@@ -35,6 +44,14 @@ namespace AI
         private void Awake()
         {
             bodyCollider = GetComponent<Collider2D>();
+            var lc = FindObjectOfType<LevelController>();
+            lc.PlayerSpawned.AddListener(onPlayerLoaded);
+            
+        }
+
+        private void onPlayerLoaded(CharacterController controller)
+        {
+            targetPlayer = GameObject.FindWithTag("Player");
         }
 
         private void Start()
@@ -51,51 +68,71 @@ namespace AI
         {
             Move();
             UpdateDirection();
+            BoxCastPlayerDetection();
         }
-
+        
         private void Move()
         {
-            Vector3 currentPosition = transform.position;
-            if (shouldMove || !isAttacking)
+            var currentPosition = transform.position;
+            switch (shouldMove)
             {
-                // How far away are we from the target?
-                float distance = (patrolPoints[currentPoint] - (Vector2)currentPosition).magnitude;
-
-                // If we are closer to our target than our minimum distance...
-                if (distance <= stopDistance)
+                case true when !isPlayerInRange:
                 {
-                    // Update to the next target
-                    currentPoint = currentPoint + 1;
-                    // If we've gone past the end of our list...
-                    // (if our current point index is equal or bigger than
-                    // the length of our list)
-                    
-                    if (currentPoint >= patrolPoints.Length)
-                    {
-                        // ...loop back to the start by setting 
-                        // the current point index to 0
-                        currentPoint = 0;
-                    }
+                    // How far away are we from the target?
+                    var distance = (patrolPoints[currentPoint] - (Vector2)currentPosition).magnitude;
 
-                    shouldMove = false;
-                    animator.SetBool("IsMoving", false);
+                    // If we are closer to our target than our minimum distance...
+                    if (distance <= stopDistance)
+                    {
+                        // Update to the next target
+                        currentPoint += 1;
+                        
+                        // If we've gone past the end of our list...
+                        // (if our current point index is equal or bigger than
+                        // the length of our list)
+                        if (currentPoint >= patrolPoints.Length)
+                        {
+                            // ...loop back to the start by setting 
+                            // the current point index to 0
+                            currentPoint = 0;
+                        }
+                        shouldMove = false;
+                        animator.SetBool("IsMoving", false);
+                        break;
+                    }
+                    
+                    if (shouldMove && !isPlayerInRange)
+                    {
+                        // Now, move in the direction of our target
+                        // Get the direction    
+                        // Subtract the current position from the target position to get a distance vector
+                        // Normalise changes it to be length 1, so we can then multiply it by our speed / force
+                        direction = (patrolPoints[currentPoint] - (Vector2)currentPosition).normalized;
+                        animator.SetBool("IsMoving", true);
+                        Vector2 targetPosition = new Vector2(patrolPoints[currentPoint].x, currentPosition.y);
+                        currentPosition = Vector2.MoveTowards(currentPosition, targetPosition, moveVelocity * Time.deltaTime);
+                        transform.position = currentPosition;
+                    }
+                    break;
+                }
+                case true when isPlayerInRange:
+                {
+                    var position = transform.position;
+                    Vector2 targetPosition = new Vector2(targetPlayer.transform.position.x, position.y);
+                    var distance = (targetPosition - (Vector2)position).magnitude;
+                    direction = (targetPosition - (Vector2)position).normalized;
+                    Debug.Log(distance.ToString());
+                    position = Vector2.MoveTowards(position, targetPosition, moveVelocity * Time.deltaTime);
+                    transform.position = position;
+                    if (distance >= boxCastSize.x - 0.09f * boxCastSize.x)
+                    {
+                        isPlayerInRange = false;
+                    }
+                    break;
                 }
             }
 
-            if (shouldMove)
-            {
-                // Now, move in the direction of our target
-                // Get the direction
-                // Subtract the current position from the target position to get a distance vector
-                // Normalise changes it to be length 1, so we can then multiply it by our speed / force
-                direction = (patrolPoints[currentPoint] - (Vector2)currentPosition).normalized;
-                animator.SetBool("IsMoving", true);
-                Vector2 targetPosition = new Vector2(patrolPoints[currentPoint].x, currentPosition.y);
-                currentPosition = Vector2.MoveTowards(currentPosition, targetPosition, moveVelocity * Time.deltaTime);
-                transform.position = currentPosition;
-            }
-
-            if (!shouldMove && !isAtWaypointStop)
+            if (!shouldMove && !isAtWaypointStop && !isAttacking)
             {
                 StartCoroutine(WayPointStopTime());
             }
@@ -118,9 +155,30 @@ namespace AI
                 {
                     puppet.localScale = Vector2.one;
                 }
+            } 
+        }
+        private void BoxCastPlayerDetection()
+        {
+            boxCastHit = Physics2D.BoxCast(transform.position + new Vector3(boxCastSize.x * 0.5f * direction.x, -boxCastSize.y, 0), boxCastSize, 0, direction);
+            
+            if (boxCastHit.collider.gameObject.CompareTag("Player"))
+            { 
+                Debug.Log("Player in range");
+                animator.SetBool("IsMoving", true);
+                isPlayerInRange = true;
+                shouldMove = true;
             }
         }
 
+        private void OnTriggerStay2D(Collider2D other)
+        {
+            if ((lightMask & 1 << other.gameObject.layer) == 1 << other.gameObject.layer)
+            {
+                Debug.Log("Enemy is in the light");
+                Destroy(gameObject);
+            }
+            
+        }
 
         private void OnCollisionEnter2D(Collision2D col)
         {
@@ -158,15 +216,18 @@ namespace AI
         {
         }
 
-        private void OnDrawGizmosSelected()
+        private void OnDrawGizmos()
         {
             if (patrolPoints.Length == 0) return;
             foreach (Vector3 pos in patrolPoints)
             {
                 var gizPos = pos;
                 gizPos.y = transform.position.y;
-                    Gizmos.DrawWireCube(gizPos, Vector3.one);
+                Gizmos.DrawWireCube(gizPos, Vector3.one);
             }
+            
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireCube(transform.position + new Vector3(boxCastSize.x * 0.5f * direction.x, boxCastSize.y * -0.5f, 0), boxCastSize);
         }
     }
 }
--- Assets/Scripts/Core/SceneManager/PlayerSpawnPoint.cs
+++ Assets/Scripts/Core/SceneManager/PlayerSpawnPoint.cs
@@ -9,16 +9,19 @@
 
 using UnityEngine;
 
-public class PlayerSpawnPoint : MonoBehaviour
+namespace Core.SceneManager
 {
-    public enum FacingDirection
+    public class PlayerSpawnPoint : MonoBehaviour
     {
-        Left, 
-        Right
+        public enum FacingDirection
+        {
+            Left, 
+            Right
+        }
+        public Vector2 GetPosition => gameObject.transform.position;
+        public FacingDirection GetFacingDirection => facingDirection;
+        public int GetSpawnIndex => spawnIndex;
+        [SerializeField] private int spawnIndex;
+        [SerializeField] private FacingDirection facingDirection;
     }
-    public Vector2 GetPosition => gameObject.transform.position;
-    public FacingDirection GetFacingDirection => facingDirection;
-    public int GetSpawnIndex => spawnIndex;
-    [SerializeField] private int spawnIndex;
-    [SerializeField] private FacingDirection facingDirection;
 }
