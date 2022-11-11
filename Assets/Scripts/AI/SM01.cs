2cbbabc8566675192b825460d673d3e88228fbc3
William Clark <williamclark10090@gmail.com>
2022-11-11 14:42:00 +1300

    SM01 Attack

--- Assets/Scripts/AI/SM01.cs
+++ Assets/Scripts/AI/SM01.cs
@@ -1,52 +1,49 @@
-using System;
 using System.Collections;
-using Character;
 using Core;
-using Unity.Mathematics;
 using UnityEngine;
-using UnityEngine.Events;
-using UnityEngine.Rendering.Universal;
 using Random = UnityEngine.Random;
+
+
+// TODO: AUDIO FX
 
 namespace AI
 {
     public class SM01 : MonoBehaviour, ILightResponder
     {
+        [Header("Movement")]
         [SerializeField] private float moveVelocity = 10;
         [SerializeField] private float stopDistance = 5;
         [SerializeField] private float maxWaypointStopTime = 3;
-        [SerializeField] private float attackCooldown = 3;
         [SerializeField] private Vector2[] patrolPoints;
+        [SerializeField] private Vector2 chaseCastSize;
+        private RaycastHit2D boxCastHit;
+        private Vector3 startPosition;
+        private Vector2 direction = new (-1, 1);
+        private bool shouldMove = true;
+        private bool isAtWaypointStop;
+        private bool isPlayerInChaseRange;
+        private int currentPoint;
+
+        [Header("Combat")]
+        [SerializeField] public float attackDistance;
+        [SerializeField] private float attackCooldown = 3;
+        private GameObject targetPlayer;
+        private SM01AttackCollider attackCollider;
+        private bool isAttacking;
+        private bool isInAttackCooldown;
+        
+        [Header("Components")]
         [SerializeField] public Animator animator;
         [SerializeField] private Transform puppet;
         [SerializeField] private LayerMask lightMask;
-        private Vector3 startPosition;
-
-        [SerializeField] private Vector2 boxCastSize;
-
-        private Collider2D bodyCollider;
-
-        [Header("Movement")]
-        private Vector2 direction = new (-1, 1);
-        private bool shouldMove = true;
-
-        private RaycastHit2D boxCastHit;
-
-        private GameObject targetPlayer;
-
-        private bool isPlayerInRange = false;
-        private bool isAttacking;
-        private bool isAtWaypointStop;
-        private int currentPoint;
         private readonly Vector3 flippedScale = new(-1, 1, 1);
 
         // Start is called before the first frame update
         private void Awake()
         {
-            bodyCollider = GetComponent<Collider2D>();
+            attackCollider = GetComponentInChildren<SM01AttackCollider>();
             var lc = FindObjectOfType<LevelController>();
             lc.PlayerSpawned.AddListener(onPlayerLoaded);
-            
         }
 
         private void onPlayerLoaded(CharacterController controller)
@@ -76,7 +73,7 @@ namespace AI
             var currentPosition = transform.position;
             switch (shouldMove)
             {
-                case true when !isPlayerInRange:
+                case true when !isPlayerInChaseRange:
                 {
                     // How far away are we from the target?
                     var distance = (patrolPoints[currentPoint] - (Vector2)currentPosition).magnitude;
@@ -101,7 +98,7 @@ namespace AI
                         break;
                     }
                     
-                    if (shouldMove && !isPlayerInRange)
+                    if (shouldMove && !isPlayerInChaseRange)
                     {
                         // Now, move in the direction of our target
                         // Get the direction    
@@ -115,19 +112,31 @@ namespace AI
                     }
                     break;
                 }
-                case true when isPlayerInRange:
+                case true when isPlayerInChaseRange:
                 {
                     var position = transform.position;
                     Vector2 targetPosition = new Vector2(targetPlayer.transform.position.x, position.y);
                     var distance = (targetPosition - (Vector2)position).magnitude;
-                    direction = (targetPosition - (Vector2)position).normalized;
-                    Debug.Log(distance.ToString());
-                    position = Vector2.MoveTowards(position, targetPosition, moveVelocity * Time.deltaTime);
-                    transform.position = position;
-                    if (distance >= boxCastSize.x - 0.09f * boxCastSize.x)
+                    
+                    if (distance <= attackDistance && !isInAttackCooldown)
                     {
-                        isPlayerInRange = false;
+                        Attack();
                     }
+                    
+                    if (!isAttacking)
+                    {
+
+                        direction = (targetPosition - (Vector2)position).normalized;
+                        position = Vector2.MoveTowards(position, targetPosition, moveVelocity * Time.deltaTime);
+                        transform.position = position;
+                    
+                        // Stop Chasing when Player Exceeds Chase Distance
+                        if (distance >= chaseCastSize.x - 0.09f * chaseCastSize.x)
+                        {
+                            isPlayerInChaseRange = false;
+                        }   
+                    }
+
                     break;
                 }
             }
@@ -136,6 +145,15 @@ namespace AI
             {
                 StartCoroutine(WayPointStopTime());
             }
+        }
+
+
+        private void Attack()
+        {
+            shouldMove = false;
+            isAttacking = true;
+            animator.SetTrigger("Melee");
+            isInAttackCooldown = true;
         }
 
         private void UpdateDirection()
@@ -159,39 +177,23 @@ namespace AI
         }
         private void BoxCastPlayerDetection()
         {
-            boxCastHit = Physics2D.BoxCast(transform.position + new Vector3(boxCastSize.x * 0.5f * direction.x, -boxCastSize.y, 0), boxCastSize, 0, direction);
-            
-            if (boxCastHit.collider.gameObject.CompareTag("Player"))
-            { 
-                Debug.Log("Player in range");
+            boxCastHit = Physics2D.BoxCast(transform.position + new Vector3(chaseCastSize.x * 0.5f * direction.x, -chaseCastSize.y, 0), chaseCastSize, 0, direction);
+
+            if (!boxCastHit.collider.gameObject.CompareTag("Player")) return;
+            if (!isInAttackCooldown)
+            {
                 animator.SetBool("IsMoving", true);
-                isPlayerInRange = true;
-                shouldMove = true;
             }
+            isPlayerInChaseRange = true;
+            shouldMove = true;
         }
 
-        private void OnTriggerStay2D(Collider2D other)
-        {
-            if ((lightMask & 1 << other.gameObject.layer) == 1 << other.gameObject.layer)
-            {
-                Debug.Log("Enemy is in the light");
-                Destroy(gameObject);
-            }
-            
-        }
-
-        private void OnCollisionEnter2D(Collision2D col)
-        {
-            var cc = col.gameObject.GetComponent<CharacterSanity>();
-            if (cc)
-            {
-                cc.DecreaseSanity(100f, false);
-            }
-        }
-
-        private IEnumerator AttackCooldownReset()
+        public IEnumerator AttackCooldownReset()
         {
             yield return new WaitForSeconds(attackCooldown);
+            attackCollider.DeactivateCollisionBox();
+            isInAttackCooldown = false;
+            isAttacking = false;
         }
         
         private IEnumerator WayPointStopTime()
@@ -204,17 +206,15 @@ namespace AI
 
         public void OnLightEntered(float intensity)
         {
-            Debug.Log("Enemy is in the light");
+            // TODO: DEATH SFX
             Destroy(gameObject);
         }
 
         public void OnLightExited(float intensity)
-        {
-        }
+        { }
 
         public void OnLightStay(float intensity)
-        {
-        }
+        { }
 
         private void OnDrawGizmos()
         {
@@ -227,7 +227,7 @@ namespace AI
             }
             
             Gizmos.color = Color.red;
-            Gizmos.DrawWireCube(transform.position + new Vector3(boxCastSize.x * 0.5f * direction.x, boxCastSize.y * -0.5f, 0), boxCastSize);
+            Gizmos.DrawWireCube(transform.position + new Vector3(chaseCastSize.x * 0.5f * direction.x, chaseCastSize.y * -0.5f, 0), chaseCastSize);
         }
     }
 }
