a570722284a4ed294d6bdeba751341ffe1ffc171
SamBlakely47 <blakelysam379@gmail.com>
2022-11-18 03:42:32 +1300

    Audio Menu
    - Created Three sliders for Audio levels. Accessible from the main menu. May need adjusted for volume on mixer itself

---
+++ Assets/Scripts/UI/AudioMenu.cs
@@ -0,0 +1,32 @@
+﻿using Audio;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioMenu : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI masterVolumeValueText;
+    [SerializeField] private Slider masterSlider;
+    [SerializeField] private TextMeshProUGUI bgmVolumeValueText;
+    [SerializeField] private Slider bgmSlider;
+    [SerializeField] private TextMeshProUGUI fxVolumeValueText;
+    [SerializeField] private Slider fxSlider;
+
+public void MasterValueChanged() // Hooked up in editor
+    {
+        AudioManager.Instance.MainAudioMixer.SetFloat("MasterVolume", Mathf.Log(masterSlider.value) * 20);
+        masterVolumeValueText.text = Mathf.RoundToInt(masterSlider.value * 100).ToString();
+    }
+
+    public void BackgroundVolumeChanged() // Hooked up in editor
+    {
+        AudioManager.Instance.MainAudioMixer.SetFloat("BGMVolume", Mathf.Log(bgmSlider.value) * 20);
+        bgmVolumeValueText.text = Mathf.RoundToInt(bgmSlider.value * 100).ToString();
+    }
+    
+    public void FXVolumeChanged() // Hooked up in editor
+    {
+        AudioManager.Instance.MainAudioMixer.SetFloat("FXVolume", Mathf.Log(fxSlider.value) * 20);
+        fxVolumeValueText.text = Mathf.RoundToInt(fxSlider.value * 100).ToString();
+    }
+}
