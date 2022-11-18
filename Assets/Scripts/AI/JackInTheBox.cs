c6d1a28bdf6f74eaf370568b6851bf51b0048b64
SamBlakely47 <blakelysam379@gmail.com>
2022-11-18 16:58:20 +1300

    Animated Jack in the box

--- Assets/Scripts/AI/JackInTheBox.cs
+++ Assets/Scripts/AI/JackInTheBox.cs
@@ -12,26 +12,27 @@ public class JackInTheBox : MovableObject
     [SerializeField] private Vector2 destination;
     [SerializeField] private float popSpeed = 1f;
     [SerializeField] private float sanityDamage = 20f;
+    [SerializeField] private Animator animator;
 
-    private CharacterController cc;
+    private CharacterController ccontroller;
 
     protected override void Start()
     {
         base.Start();
-        cc = FindObjectOfType<CharacterController>();
+        ccontroller = FindObjectOfType<CharacterController>();
     }
 
     protected override void Update()
     {
         base.Update();
         
-        if (jack.activeInHierarchy)
+        /*if (jack.activeInHierarchy)
         {
             jack.transform.position = Vector2.Lerp(jack.transform.position, new Vector2(transform.position.x, destination.y), Time.deltaTime * popSpeed);
-        }
+        }*/
     }
 
-    protected override void Interact(CharacterController cc)
+    protected override void Interact(CharacterController controller)
     {
         DisablePrompt();
         StartCoroutine(JacksBoxyRoutine());
@@ -51,12 +52,26 @@ public class JackInTheBox : MovableObject
 
     private void SayBoo()
     {
-        AudioManager.Instance.PlaySound(boo,1f);
-        if (cc != null)
+        if (animator != null)
         {
-            cc.GetCharacterSanity.DecreaseSanity(sanityDamage, false);
+            animator.SetTrigger("Trigger");
+            //StartCoroutine(AnimatorRoutine());
+            //animator.SetBool("Jumping", false);
+        }
+        animator.SetBool("Stop", true);
+        AudioManager.Instance.PlaySound(boo,1f);
+        if (ccontroller != null)
+        {
+            ccontroller.GetCharacterSanity.DecreaseSanity(sanityDamage, false);
             Debug.Log("Sanity damaged");
         }
         
     }
+
+    /*private IEnumerator AnimatorRoutine()
+    {
+        //yield return new WaitForSeconds(3f);
+        
+    }*/
+    
 }
